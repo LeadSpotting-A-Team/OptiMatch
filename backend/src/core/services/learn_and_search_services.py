185a156370a6a52b9a0d12cf0f130b343cef9a30
TmiraOhana <TMIRA229@gmail.com>
2026-04-16 10:55:59 +0300

    refactor(learn_service): streamline face learning process by removing unused variables and enhancing error handling, returning the count of valid faces learned

--- backend/src/core/services/learn_and_search_services.py
+++ backend/src/core/services/learn_and_search_services.py
@@ -17,33 +17,27 @@ import numpy as np
 
 
 #this function is used to learn the faces from the csv file and add them to the database and ivf
-#this function returns the number of valid posts that were learned.
+#this function returns the number of valid faces that were learned.
 def learn_service(csv_file: IO[str] , detector: IFaceDetector , embedding_model: IEmbeddingModel , vector_store: IVectorStore , metadata_repository: IMetadataRepository) -> int:
     
-    valid_post_count = 0
+    valid_face_count = 0
     
     posts = read_posts_from_csv(csv_file)
-    valid_embeddings = []
-    valid_face_ids = []
     for post in posts:
         try:
             #ingest_post -> [{"face_id": face_id, "face_image": face_image} , {"face_id": face_id2, "face_image": face_image2}  , ....]
             list_of_faces_dicts = ingest_post(post, detector, metadata_repository);
             for d in list_of_faces_dicts:
-                face_id, face_image = d["face_id"], d["face_image"]
-                emb = embedding_model.compute_embedding(embedding_model.preprocess(face_image))
-                if emb is None:
-                    continue
-                valid_embeddings.append(emb)
-                valid_face_ids.append(face_id)
-            valid_post_count += 1;
-
-
+                try:
+                    face_id, face_image = d["face_id"], d["face_image"]
+                    emb = embedding_model.compute_embedding(embedding_model.preprocess(face_image))
+                    if emb is not None:
+                        vector_store.add_face(emb, face_id)
+                        valid_face_count += 1
+                except Exception as e:
+                    pass # Ignore errors
         except Exception as e:#to skip the post if there is an error
             continue
 
 
-    if valid_embeddings:
-        vector_store.add_faces_batch(np.vstack(valid_embeddings), valid_face_ids)
-
-    return valid_post_count
+    return valid_face_count
